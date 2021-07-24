46c7a6331ceaffe76ea8c9101fc13d5d4b0d09a8
solletiharshitha <solletiharshitha@gmail.com>
2021-07-25 00:26:02 +0530

    [Harshitha] Read all lines in file

--- FileIOOperationsDemo/Program.cs
+++ FileIOOperationsDemo/Program.cs
@@ -8,26 +8,54 @@ namespace FileIOOperationsDemo
         static void Main(string[] args)
         {
             string filePath = @"D:\tvstraining\FileIOOperationsDemo\FileIOOperationsDemo\SampleData.txt";
-            FileExists(filePath);
+            //FileExists(filePath);
+            ReadLineByLine(filePath);
         }
         /// <summary>
         /// Checking File Exists or Not
         /// </summary>
         /// <param name="filePath"></param>
-        public static void FileExists(string filePath)
+        public static bool FileExists(string filePath)
         {
             try
             {
                 if (File.Exists(filePath))
                 {
                     Console.WriteLine("File exists");
+                    return true;
                 }
                 else
                 {
                     Console.WriteLine("File does not exists");
+                    return false;
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Read Line By Line
+        /// </summary>
+        /// <param name="filePath"></param>
+        public static void ReadLineByLine(string filePath)
+        {
+            try
+            {
+                if (FileExists(filePath))
+                {
+                    string[] lines = File.ReadAllLines(filePath);
+                    foreach(string line in lines)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+                
+            }
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
