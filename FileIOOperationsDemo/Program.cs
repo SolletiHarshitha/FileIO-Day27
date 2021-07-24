96dafa1d72ad535c2c9200f6bf0eb478c3aa4a5b
solletiharshitha <solletiharshitha@gmail.com>
2021-07-25 00:16:03 +0530

    [Harshitha] Checking File Exists or Not

---
+++ FileIOOperationsDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace FileIOOperationsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"D:\tvstraining\FileIOOperationsDemo\FileIOOperationsDemo\SampleData.txt";
+            FileExists(filePath);
+        }
+        public static void FileExists(string filePath)
+        {
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    Console.WriteLine("File exists");
+                }
+                else
+                {
+                    Console.WriteLine("File does not exists");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
